bedae8faf5ab055d3e8739ca1a56d0761d4574b9
gjpin <3874515+gjpin@users.noreply.github.com>
2023-07-22 23:20:19 +0100

    do not autosync saves of games with cloud sync

--- setup.sh
+++ setup.sh
@@ -69,11 +69,8 @@ systemctl --user enable --now syncthing.service
 tee ${HOME}/.local/bin/backup-heroic-saves.sh << EOF
 #!/usr/bin/bash
 rsync -r "/home/deck/Games/Heroic/Prefixes/default/Remnant From the Ashes/pfx/drive_c/users/steamuser/AppData/Local/Remnant/Saved/SaveGames"/. /home/deck/syncthing/saves/heroic/remnant_from_the_ashes
-rsync -r "/home/deck/Games/Heroic/Prefixes/default/The Pathless/pfx/drive_c/users/steamuser/AppData/Local/Pathless/Saved/SaveGames"/. /home/deck/syncthing/saves/heroic/the_pathless
-rsync -r "/home/deck/Games/Heroic/Prefixes/default/Solar Ash/pfx/drive_c/users/steamuser/AppData/Local/Solar/EGS/619ea35adadd4d6cb8fea37060af796c/SaveGames"/. /home/deck/syncthing/saves/heroic/solar_ash
 rsync -r "/home/deck/Games/Heroic/Prefixes/default/Wonder Boy The Dragons Trap/pfx/drive_c/users/steamuser/AppData/Local/Lizardcube/The Dragon's Trap"/. /home/deck/syncthing/saves/heroic/wonder_boy_the_dragons_trap
 rsync -r "/home/deck/Games/Heroic/Prefixes/default/Shadow Tactics Blades of the Shogun/pfx/drive_c/users/steamuser/AppData/Local/Daedalic Entertainment GmbH/Shadow Tactics Blades of the Shogun Aikos Choice/user"/. /home/deck/syncthing/saves/heroic/shadow_tactics_blades_of_the_shogun_aikos_choice
-rsync -r "/home/deck/Games/Heroic/Prefixes/default/Scourgebringer/pfx/drive_c/users/steamuser/AppData/LocalLow/Flying Oak Games/ScourgeBringer"/. /home/deck/syncthing/saves/heroic/scourgebringer
 rsync -r "/home/deck/Games/Heroic/Prefixes/default/Void Bastards/pfx/drive_c/users/steamuser/AppData/LocalLow/BlueManchu/VoidBastards"/. /home/deck/syncthing/saves/heroic/void_bastards
 rsync -r "/home/deck/Games/Heroic/Prefixes/default/Cris Tales/pfx/drive_c/users/steamuser/AppData/LocalLow/Dreams Uncorpored LLC/Cristales"/. /home/deck/syncthing/saves/heroic/cris_tales
 rsync -r "/home/deck/Games/Heroic/Prefixes/default/Dandara Trials of Fear Edition/pfx/drive_c/users/steamuser/AppData/LocalLow/Long Hat House/Dandara"/. /home/deck/syncthing/saves/heroic/dandara_trials_of_fear_edition
@@ -93,11 +90,8 @@ Description="Monitor the Heroic save directories for changes"
 
 [Path]
 PathChanged=/home/deck/Games/Heroic/Prefixes/default/Remnant From the Ashes/pfx/drive_c/users/steamuser/AppData/Local/Remnant/Saved/SaveGames
-PathChanged=/home/deck/Games/Heroic/Prefixes/default/The Pathless/pfx/drive_c/users/steamuser/AppData/Local/Pathless/Saved/SaveGames
-PathChanged=/home/deck/Games/Heroic/Prefixes/default/Solar Ash/pfx/drive_c/users/steamuser/AppData/Local/Solar/EGS/619ea35adadd4d6cb8fea37060af796c/SaveGames
 PathChanged=/home/deck/Games/Heroic/Prefixes/default/Wonder Boy The Dragons Trap/pfx/drive_c/users/steamuser/AppData/Local/Lizardcube/The Dragon's Trap
 PathChanged=/home/deck/Games/Heroic/Prefixes/default/Shadow Tactics Blades of the Shogun/pfx/drive_c/users/steamuser/AppData/Local/Daedalic Entertainment GmbH/Shadow Tactics Blades of the Shogun Aikos Choice/user
-PathChanged=/home/deck/Games/Heroic/Prefixes/default/Scourgebringer/pfx/drive_c/users/steamuser/AppData/LocalLow/Flying Oak Games/ScourgeBringer
 PathChanged=/home/deck/Games/Heroic/Prefixes/default/Void Bastards/pfx/drive_c/users/steamuser/AppData/LocalLow/BlueManchu/VoidBastards
 PathChanged=/home/deck/Games/Heroic/Prefixes/default/Cris Tales/pfx/drive_c/users/steamuser/AppData/LocalLow/Dreams Uncorpored LLC/Cristales
 PathChanged=/home/deck/Games/Heroic/Prefixes/default/Dandara Trials of Fear Edition/pfx/drive_c/users/steamuser/AppData/LocalLow/Long Hat House/Dandara
@@ -131,11 +125,9 @@ systemctl --user enable --now backup-heroic-saves.path
 tee ${HOME}/.local/bin/restore-heroic-saves.sh << EOF
 #!/usr/bin/bash
 rsync -r --mkpath "/home/deck/syncthing/saves/heroic/remnant_from_the_ashes/" "/home/deck/Games/Heroic/Prefixes/default/Remnant From the Ashes/pfx/drive_c/users/steamuser/AppData/Local/Remnant/Saved/SaveGames"
-rsync -r --mkpath "/home/deck/syncthing/saves/heroic/the_pathless/" "/home/deck/Games/Heroic/Prefixes/default/The Pathless/pfx/drive_c/users/steamuser/AppData/Local/Pathless/Saved/SaveGames"
 rsync -r --mkpath "/home/deck/syncthing/saves/heroic/solar_ash/" "/home/deck/Games/Heroic/Prefixes/default/Solar Ash/pfx/drive_c/users/steamuser/AppData/Local/Solar/EGS/619ea35adadd4d6cb8fea37060af796c/SaveGames"
 rsync -r --mkpath "/home/deck/syncthing/saves/heroic/wonder_boy_the_dragons_trap/" "/home/deck/Games/Heroic/Prefixes/default/Wonder Boy The Dragons Trap/pfx/drive_c/users/steamuser/AppData/Local/Lizardcube/The Dragon's Trap"
 rsync -r --mkpath "/home/deck/syncthing/heroic/shadow_tactics_blades_of_the_shogun_aikos_choice/" "/home/deck/Games/Heroic/Prefixes/default/Shadow Tactics Blades of the Shogun/pfx/drive_c/users/steamuser/AppData/Local/Daedalic Entertainment GmbH/Shadow Tactics Blades of the Shogun Aikos Choice/user"
-rsync -r --mkpath "/home/deck/syncthing/heroic/scourgebringer/" "/home/deck/Games/Heroic/Prefixes/default/Scourgebringer/pfx/drive_c/users/steamuser/AppData/LocalLow/Flying Oak Games/ScourgeBringer"
 rsync -r --mkpath "/home/deck/syncthing/heroic/void_bastards/" "/home/deck/Games/Heroic/Prefixes/default/Void Bastards/pfx/drive_c/users/steamuser/AppData/LocalLow/BlueManchu/VoidBastards"
 rsync -r --mkpath "/home/deck/syncthing/heroic/cris_tales/" "/home/deck/Games/Heroic/Prefixes/default/Cris Tales/pfx/drive_c/users/steamuser/AppData/LocalLow/Dreams Uncorpored LLC/Cristales"
 rsync -r --mkpath "/home/deck/syncthing/heroic/dandara_trials_of_fear_edition/" "/home/deck/Games/Heroic/Prefixes/default/Dandara Trials of Fear Edition/pfx/drive_c/users/steamuser/AppData/LocalLow/Long Hat House/Dandara"
@@ -155,11 +147,9 @@ Description="Monitor the Heroic save directories for changes"
 
 [Path]
 PathChanged=/home/deck/syncthing/saves/heroic/remnant_from_the_ashes
-PathChanged=/home/deck/syncthing/saves/heroic/the_pathless
 PathChanged=/home/deck/syncthing/saves/heroic/solar_ash
 PathChanged=/home/deck/syncthing/saves/heroic/wonder_boy_the_dragons_trap
 PathChanged=/home/deck/syncthing/saves/heroic/shadow_tactics_blades_of_the_shogun_aikos_choice
-PathChanged=/home/deck/syncthing/saves/heroic/scourgebringer
 PathChanged=/home/deck/syncthing/saves/heroic/void_bastards
 PathChanged=/home/deck/syncthing/saves/heroic/cris_tales
 PathChanged=/home/deck/syncthing/saves/heroic/dandara_trials_of_fear_edition
