18f051edc7354bc9af06363bd91b9d666058139c
gjpin <3874515+gjpin@users.noreply.github.com>
2025-08-07 16:35:13 +0100

    add ludusavi

--- setup.sh
+++ setup.sh
@@ -278,3 +278,39 @@ WantedBy=default.target
 EOF
 
 systemctl --user enable --now syncthing.service
+
+################################################
+##### Ludusavi
+################################################
+
+# Install Ludusavi
+flatpak install -y flathub com.github.mtkennerly.ludusavi
+curl https://raw.githubusercontent.com/gjpin/steam-deck/main/configs/flatpak/com.github.mtkennerly.ludusavi -o ${HOME}/.local/share/flatpak/overrides/com.github.mtkennerly.ludusavi
+
+# Import Ludusavi config
+mkdir -p ${HOME}/.var/app/com.github.mtkennerly.ludusavi/config/ludusavi
+curl https://raw.githubusercontent.com/gjpin/steam-deck/main/configs/ludusavi/config.yaml | envsubst > ${HOME}/.var/app/com.github.mtkennerly.ludusavi/config/ludusavi/config.yaml
+
+# Set automatic backups
+# https://github.com/mtkennerly/ludusavi/blob/master/docs/help/backup-automation.md
+tee ~/.config/systemd/user/ludusavi-backup.service << 'EOF'
+[Unit]
+Description="Ludusavi backup"
+
+[Service]
+ExecStart=/usr/bin/flatpak run com.github.mtkennerly.ludusavi backup --force
+EOF
+
+tee ~/.config/systemd/user/ludusavi-backup.timer << 'EOF'
+[Unit]
+Description="Ludusavi backup timer"
+
+[Timer]
+OnCalendar=*:0/5
+Unit=ludusavi-backup.service
+
+[Install]
+WantedBy=timers.target
+EOF
+
+systemctl --user enable ${HOME}/.config/systemd/user/ludusavi-backup.timer
