a767e9a87457bdbc1f08aa3bcf4ba2ca5034581e
gjpin <3874515+gjpin@users.noreply.github.com>
2025-11-24 00:01:18 +0000

    ludusavi / heroic: restore/backup automatically

--- modules/20-applications/20-ludusavi.sh
+++ modules/20-applications/20-ludusavi.sh
@@ -12,29 +12,58 @@ cp ./configs/flatpak/com.github.mtkennerly.ludusavi ${HOME}/.local/share/flatpak
 mkdir -p ${HOME}/.var/app/com.github.mtkennerly.ludusavi/config/ludusavi
 cat ./configs/ludusavi/config.yaml | envsubst > ${HOME}/.var/app/com.github.mtkennerly.ludusavi/config/ludusavi/config.yaml
 
-# Set automatic backups
+# Set automatic backups and restores when Heroic is opened/closed
 # https://github.com/mtkennerly/ludusavi/blob/master/docs/help/backup-automation.md
-tee ~/.config/systemd/user/ludusavi-backup.service << 'EOF'
+# https://github.com/mtkennerly/ludusavi/blob/master/docs/cli.md
+# Create 'heroic' watcher script
+tee ${HOME}/.local/bin/ludusavi-heroic.sh << 'EOF'
+#!/usr/bin/bash
+
+PROCESS="heroic"
+
+while true; do
+    # Wait for Heroic to start
+    while ! pgrep -x "$PROCESS" >/dev/null; do
+        sleep 1
+    done
+
+    # --- RUN ON START ---
+    # Restore save games with ludusavi
+    /usr/bin/flatpak run com.github.mtkennerly.ludusavi restore --force
+
+    # Wait for Heroic to exit
+    while pgrep -x "$PROCESS" >/dev/null; do
+        sleep 1
+    done
+
+    # --- RUN ON EXIT ---
+    # Backup save games with ludusavi
+    /usr/bin/flatpak run com.github.mtkennerly.ludusavi backup --force
+
+    # Loop again to watch for next launch
+done
+EOF
+
+chmod +x ${HOME}/.local/bin/ludusavi-heroic.sh
+
+# Create systemd service
+tee ${HOME}/.config/systemd/user/ludusavi-heroic.service << EOF
 [Unit]
-Description="Ludusavi backup"
+Description=Heroic start/stop watcher
 
 [Service]
-ExecStart=/usr/bin/flatpak run com.github.mtkennerly.ludusavi backup --force
-EOF
-
-tee ~/.config/systemd/user/ludusavi-backup.timer << 'EOF'
-[Unit]
-Description="Ludusavi backup timer"
-
-[Timer]
-OnCalendar=*:0/5
-Unit=ludusavi-backup.service
+Type=simple
+ExecStart=%h/.local/bin/ludusavi-heroic.sh
+Restart=always
+RestartSec=1
 
 [Install]
-WantedBy=timers.target
+WantedBy=default.target
 EOF
 
-systemctl --user enable ${HOME}/.config/systemd/user/ludusavi-backup.timer
+# Enable systemd service
+systemctl --user daemon-reload
+systemctl --user enable ludusavi-heroic.service
 
 log_success "Module completed successfully"
 log_end
